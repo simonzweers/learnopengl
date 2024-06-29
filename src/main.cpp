16e5eab6de753ef832c828bf78a4c99549ab0f2a
Simon Zweers <s.zweers21@gmail.com>
2024-06-29 10:58:46 +0200

    Draw single color to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,8 @@ int main() {
         processInput(window);
 
         // Rendering
-        //...
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         // Check abd call events and swap the buffers
         glfwSwapBuffers(window);
