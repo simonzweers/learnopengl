6efa25d413d37279ef8e99511f466a1d1930f742
Simon Zweers <simon.zweers@hva.nl>
2024-12-23 00:38:06 +0100

    Started stb implementation for texture

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include <cstdlib>
 #include <math.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -50,6 +53,14 @@ int main() {
         {0.0f, 0.0f, 1.0f}, // rop right
     };
 
+    float texCoords[] = {
+        0.0f, 0.0f, // Lower left
+        1.0f, 0.0f, // Lower right
+        0.5f, 1.0f, // Top center
+    };
+
+    float borderColor[] = {1.0f, 1.0f, 0.0f, 1.0f};
+
     /*
        unsigned int indecies[][3] = {
     // First triangle
@@ -58,6 +69,29 @@ int main() {
     {1, 2, 3},
     };
     */
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int width, height, nrChannels;
+    unsigned char *image_data =
+        stbi_load("res/container.jpg", &width, &height, &nrChannels, 0);
+    if (image_data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                     GL_UNSIGNED_BYTE, image_data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::printf("Failed to load Texture");
+    }
+    stbi_image_free(image_data);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, image_data);
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
