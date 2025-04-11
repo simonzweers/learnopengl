d0ea8e725fd2795f4f46f1551319e62d64ab3d1c
Simon Zweers <simon.zweers@protonmail.com>
2025-04-12 01:00:38 +0200

    Shoops forgot to commit Camera.cpp

---
+++ src/Camera.cpp
@@ -0,0 +1,36 @@
+#include "Camera.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/trigonometric.hpp"
+
+#include <glm/glm.hpp>
+Camera::Camera(Shader &shader, const uint32_t width, const uint32_t height,
+               const float fov)
+    : _shader(shader), _width(width), _height(height), _fov(fov) {
+    printf("Initializing camera\n");
+
+    // Initialize model matrix
+    _model = glm::mat4(1.0f);
+    _model =
+        glm::rotate(_model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+    // Initialize view matrix
+    _view = glm::mat4(1.0f);
+    _view = glm::translate(_view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+    // Initialize projection matrix
+    _projection = glm::perspective(glm::radians(45.0f),
+                                   (float)width / (float)height, 0.1f, 100.0f);
+
+    _shader.setMat4("model", _model);
+}
+
+Camera::~Camera() {}
+
+void Camera::update() {
+    _view = glm::lookAt(_cameraPos, _cameraPos + _cameraFront, _globalUp);
+    _projection = glm::perspective(glm::radians(_fov),
+                                   (float)_width / (float)_height, 0.1f, 100.f);
+    _shader.setMat4("view", _view);
+    _shader.setMat4("projection", _projection);
+}
