de6bb4e5aa5ea8b199b47d9fb60b2aa79add5643
Simon Zweers <simon.zweers@protonmail.com>
2025-05-10 20:20:24 +0200

    Added material struct

--- shaders/lighting-fs.glsl
+++ shaders/lighting-fs.glsl
@@ -1,4 +1,14 @@
 #version 330 core
+
+struct Material {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shininess;
+};
+
+uniform Material Material;
+
 out vec4 FragColor;
   
 uniform vec3 objectColor;
@@ -13,14 +23,17 @@ in vec3 FragPos;
 void main()
 {
 
+	// Ambient
 	float ambientStrength = 0.1;
 	vec3 ambient = ambientStrength * lightColor;
 
+	// Diffuse
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * lightColor;
 
+	// Specular
 	float specularStrength = 0.5;
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
