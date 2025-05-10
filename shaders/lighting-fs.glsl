64ba25c2939929973a185e6343414b78e0ae5661
Simon Zweers <simon.zweers@protonmail.com>
2025-05-11 00:59:28 +0200

    Added light attenuation

--- shaders/lighting-fs.glsl
+++ shaders/lighting-fs.glsl
@@ -7,12 +7,16 @@ struct Material {
 };
 
 struct Light {
-	// vec3 position;
-	vec3 direction;
+	vec3 position;
+	// vec3 direction;
 
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+
+	float constant;
+	float linear;
+	float quadratic;
 };
 
 uniform Material material;
@@ -32,13 +36,19 @@ in vec2 TexCoords;
 
 void main()
 {
+	float distance = length(light.position - FragPos);
+	float attenuation = 1.0 / (
+			light.constant +
+			light.linear * distance +
+			light.quadratic * (distance * distance)
+			);
 
 	// Ambient
 	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
 	// Diffuse
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(-light.direction);
+	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
@@ -47,6 +57,10 @@ void main()
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
 	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+	ambient  *= attenuation; 
+	diffuse  *= attenuation;
+	specular *= attenuation;   
 
 	vec3 result = (ambient + diffuse + specular);
     FragColor = vec4(result, 1.0);
--- src/main.cpp
+++ src/main.cpp
@@ -324,8 +324,11 @@ int main() {
         lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
         lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        // lightingShader.setVec3("light.position", lightPos);
-        lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+        lightingShader.setVec3("light.position", lightPos);
+        // lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+        lightingShader.setFloat("light.constant", 1.0f);
+        lightingShader.setFloat("light.linear", 0.09f);
+        lightingShader.setFloat("light.quadratic", 0.032f);
 
         glBindVertexArray(cubeVAO);
         // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
